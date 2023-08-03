a0f1e5cac1c37d6e4ee48b51a101e6f1a4b11c45
Maxim Abavi <afghan1363@ya.ru>
2023-08-03 23:25:54 +0500

    add test_format_data

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,5 @@
 import os.path
+
 from modules import utils
 
 path_for_test = os.path.join("..", "datafiles", "for_test.json")
@@ -20,3 +21,9 @@ def test_load_trans_data():
         "from": "Счет 48894435694657014368",
         "to": "Счет 38976430693692818358"
     }]
+
+
+def test_format_data():
+    assert utils.format_data({"date": "2019-07-12T20:41:47.882230", "from": "Счет 48894435694657014368",
+                              "to": "Счет 38976430693692818358"}) == {"date": "12.07.2019", "from": "Счет 4889 44** **** 4368",
+                              "to": "Счет **8358"}
