e7c1ba57abb121a3cb14efbcbb92cfeb58290e17
Maxim Abavi <afghan1363@ya.ru>
2023-08-05 00:54:01 +0500

    add def format_account_number and def format_card_number

--- modules/utils.py
+++ modules/utils.py
@@ -27,6 +27,23 @@ def load_trans_data(path):
     return executed_transactions
 
 
+def format_card_number(card):
+    format_card = card.split(" ")
+    card_list = list(format_card[-1])
+    card_list[6:12] = "*" * len(card[6:12])
+    card_number = "".join(card_list)
+    format_card[-1] = ' '.join([card_number[i:i + 4] for i in range(0, len(card_number), 4)])
+    hidden_card = " ".join(format_card)
+    return hidden_card
+
+def format_account_number(account):
+    account_list = account.split(" ")
+    account_list[-1] = "*" * 2 + account_list[-1][-4:]
+    hidden_account = " ".join(account_list)
+    return hidden_account
+
+
+
 def format_data(trans_data):
     """
     Making right format to show
@@ -35,11 +52,14 @@ def format_data(trans_data):
     """
     for data in trans_data:
         parsed_date = data["date"].split("T")[0].replace("-", ".")
+
         data["date"] = parsed_date
         sender = data["from"]
-        hide_sender = sender[:-10] + "*" * sender[-10:-4] + sender [-4:]
-        hidden_sender = " ".join([hide_sender[i:i-4] for i in range(-1, -len(hide_sender), 4)]) # wrong!!!
+        hide_sender = sender[:-10] + "*" * 6 + sender [-4:]
+        hidden_sender = " ".join([hide_sender[i:i-4] for i in range(-1, -12, 4)]) # wrong!!!
         data["from"] = hidden_sender
         receiver = data["to"]
-        hidden_receiver = "*" * receiver[-6:-4] + receiver[-4:]
+        hidden_receiver = "*" * 2 + receiver[-4:]
         data["to"] = hidden_receiver
+
+
