adca44f978227a23d6e1eebb971606a5d08063e7
Maxim Abavi <afghan1363@ya.ru>
2023-08-05 03:15:20 +0500

    add def format_date

--- modules/utils.py
+++ modules/utils.py
@@ -1,15 +1,6 @@
 import json
 
 
-def key_to_sort(key):
-    """
-    For sort by needed key
-    :param key: is given in function sort()
-    :return: data in given key
-    """
-    return key["data"]
-
-
 def load_trans_data(path):
     """
     For load all executed transaction
@@ -27,6 +18,12 @@ def load_trans_data(path):
     return executed_transactions
 
 
+def format_date(date):
+    date_ext = date.split("T")[0]
+    date_formated = ".".join(date_ext.split("-")[::-1])
+    return date_formated
+
+
 def format_card_number(card):
     format_card = card.split(" ")
     card_list = list(format_card[-1])
@@ -36,6 +33,7 @@ def format_card_number(card):
     hidden_card = " ".join(format_card)
     return hidden_card
 
+
 def format_account_number(account):
     account_list = account.split(" ")
     account_list[-1] = "*" * 2 + account_list[-1][-4:]
@@ -43,23 +41,22 @@ def format_account_number(account):
     return hidden_account
 
 
-
 def format_data(trans_data):
     """
     Making right format to show
-    :param trans_data: List with transaction
+    :param trans_data: List with transactions
     :return: None
     """
+
+    def key_to_sort(key):
+        """
+        For sort by needed key
+        :param key: is given in function sort()
+        :return: data in given key
+        """
+        return key["data"]
+
+    trans_data.sort(key=key_to_sort, reverse=True)
     for data in trans_data:
         parsed_date = data["date"].split("T")[0].replace("-", ".")
-
         data["date"] = parsed_date
-        sender = data["from"]
-        hide_sender = sender[:-10] + "*" * 6 + sender [-4:]
-        hidden_sender = " ".join([hide_sender[i:i-4] for i in range(-1, -12, 4)]) # wrong!!!
-        data["from"] = hidden_sender
-        receiver = data["to"]
-        hidden_receiver = "*" * 2 + receiver[-4:]
-        data["to"] = hidden_receiver
-
-
