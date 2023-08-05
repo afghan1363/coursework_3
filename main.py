8adc811f85a0c002006d1973a1dc6a8041747666
Maxim Abavi <afghan1363@ya.ru>
2023-08-05 11:41:07 +0500

    create main.py with code

---
+++ main.py
@@ -0,0 +1,17 @@
+import os.path
+from modules.utils import load_trans_data, format_data
+
+path_to_trans_data = os.path.join("datafiles", "operations.json")
+trans_data = load_trans_data(path_to_trans_data)
+trans_data_formatted = format_data(trans_data)
+for data in range(5):
+    print(f"{trans_data_formatted[data]['date']} {trans_data_formatted[data]['description']}")
+    print(f"{trans_data_formatted[data].get('from', '')} -> {trans_data_formatted[data]['to']}")
+    print(f"{trans_data_formatted[data]['operationAmount']['amount']} \
+{trans_data_formatted[data]['operationAmount']['currency']['name']}")
+    print()
+
+# Пример вывода для одной операции:
+# 14.10.2018 Перевод организации
+# Visa Platinum 7000 79** **** 6361 -> Счет **9638
+# 82771.72 руб."
